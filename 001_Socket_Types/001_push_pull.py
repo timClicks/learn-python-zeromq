2e0897867e74a11bf288f6a5fecdfef9ce2d1757
Jaume Devesa <jaumedevesa@gmail.com>
2011-06-29 23:17:21 +0200

    Initial commit

---
+++ 001_Socket_Types/001_push_pull.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+
+# PUSH and PULL sockets work together to load balance messages going one way.
+# Multiple PULL sockets connected to a PUSH each receive messages from the PUSH.
+# ZeroMQ automatically load balances the messages between all pull sockets.
+#
+# We're going to build a simple load balanced message system that looks like this:
+#
+#                         push_sock
+#                         /       \
+#                  pull_sock1   pull_sock2
+#
+# Each socket will get its own thread, so you'll see them run simultanously
+
+import threading
+import time
+import zmq
+
+
+context = zmq.Context()
+
+class PullSocket(threading.Thread):
+
+    def __init__(self, num):
+        threading.Thread.__init__(self)
+        self.num = num
+        self.kill_recieved = False
+
+    def run(self):
+        pull_sock = context.socket(zmq.PULL)
+        time.sleep(3)
+        print "Pull " + str(self.num) + " connecting"
+        pull_sock.connect("tcp://127.0.0.1:2200")
+        try:
+            message = pull_sock.recv()
+            while message and not self.kill_recieved:
+                print "Pull" + str(self.num) + ": I recieved a message '" + message + "'"
+                message = pull_sock.recv()
+        except KeyboardInterrupt:
+            print "Socket terminated"
+
+class PushSocket(threading.Thread):
+
+    """ Here we define the Push socket. """
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.kill_recieved = False
+
+    def run(self):
+        push_sock = context.socket(zmq.PUSH)
+        push_sock.bind("tcp://127.0.0.1:2200")
+        for i in range (0, 7):
+            message = str(i + 1) + " Potato"
+            print "Sending " + message
+            message = push_sock.send(message)
+            time.sleep(1)
+    
+threads = []
+
+thread_push = PushSocket()
+thread_push.start()
+threads.append(thread_push)
+
+thread_pull = PullSocket(0)
+thread_pull.start()
+threads.append(thread_pull)
+
+thread_pull_2 = PullSocket(1)
+thread_pull_2.start()
+threads.append(thread_pull_2)
+
+thread_push.join()
+thread_pull.join()
+thread_pull_2.join()
+
+context.term()
---
+++ README.md
@@ -0,0 +1,9 @@
+# Learn ZeroMQ by Example in Python #
+
+This project attempts to be a way to learn ZeroMQ with some examples to explore its functionality. It is initally based in the [andrewvc](http://github.com/andrewvc)'s [learn-ruby-zeromq](https://github.com/andrewvc/learn-ruby-zeromq) project copying its structure and its examples trying to have the same behaviour and output but with pyhton language instead of ruby.
+
+Hope to expand the examples and be useful for other ZeroMQ learners.
+
+## About ZeroMQ ##
+
+According to its creators, [ZeroMQ] (http://www.zeromq.org) is a socket library that acts as a concurrency framework. [Ilia Grigorik](https://github.com/igrigorik) does a great job in his blog introducing its basic features. I am not able to do it better so, please check it [here](http://www.igvita.com/2010/09/03/zeromq-modern-fast-networking-stack/) if you still interested.
